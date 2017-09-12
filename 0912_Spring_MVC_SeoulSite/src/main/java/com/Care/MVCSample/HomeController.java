89d6a59e5a9633b4c9d59e1f0828f5ebd574aaa5
user10 <user10@406-10>
2017-09-12 12:57:36 +0900

    테스트

---
+++ 0912_Spring_MVC_SeoulSite/src/main/java/com/Care/MVCSample/HomeController.java
@@ -0,0 +1,45 @@
+package com.Care.MVCSample;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController { 
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/move")//value는 요청받은 url를 의미함.http: 프로토콜,localhost:도메인주소,8081:접속포트,HelloWeb:사용할 서비스,move를 포함한 이후 내용:URI
+	public String move() {//URL:http://localhost:8081/HelloWeb/move
+
+		return "move";
+	}
+	
+}
